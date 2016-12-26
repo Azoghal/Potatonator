61a70ca78640d1ef52faecbfa8e27e70658f3e20
Azoghal <sam.beer208@gmail.com>
2016-12-26 16:40:28 +0000

    sucnera

--- main.lua
+++ main.lua
@@ -2,6 +2,7 @@ function love.load()
 	character = {}
 	character.x = 50
 	character.y = 50
+	character.image = love.graphics.newImage("Potato.png")
 	-- everything we want to load into the game b4 it starts// runs only 1 time
 
 end
