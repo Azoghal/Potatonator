7a2d97e1190cdd58f749677053ceac8107b21bdc
Azoghal <sam.beer208@gmail.com>
2016-12-26 21:57:43 +0000

    sdfd

--- main.lua
+++ main.lua
@@ -8,16 +8,16 @@ function love.load()
 	function chrMove()
 
 		if love.keyboard.isDown("w") then
-			character.y = character.y - 1
+			character.y = character.y - 2
 		end
 		if love.keyboard.isDown("a") then
-			character.x = character.x - 1
+			character.x = character.x - 2
 		end
 		if love.keyboard.isDown("s") then
-			character.y = character.y + 1
+			character.y = character.y + 2
 		end
 		if love.keyboard.isDown("d") then
-			character.x = character.x + 1
+			character.x = character.x + 2
 		end
 
 	end
@@ -27,20 +27,31 @@ function love.load()
 	enemy = {}
 	enemy.x = 300
 	enemy.y = 300
+	enemy.vx = 0
+	enemy.vy = 0
+	enemy.moveCD = 0
 	enemy.image = love.graphics.newImage("Oven.png")
-
-	-- enemy move function
 
 	function enemyMove(target)
 		tx,ty = target.x,target.y
 	    angle = math.atan2(ty - enemy.y, tx - enemy.x)
 	    enemy.vx, enemy.vy = math.cos(angle), math.sin(angle)
+	    enemy.moveCD = 40
+	end
+	function enemyMove2()
+		enemy.x = enemy.x + enemy.vx
+		enemy.y = enemy.y + enemy.vy
 	end
 
 end
 
 function love.update()
 
+	enemy.moveCD = enemy.moveCD - 1
+	if enemy.moveCD <= 0 then
+		enemyMove(character)
+	end
+	enemyMove2()
 	chrMove()
 
 end
@@ -48,7 +59,7 @@ end
 function love.draw()
 
 	love.graphics.draw(character.image,character.x,character.y)
-	love.graphics.draw(enemy.image,enemy.x,enemy.y)
+	love.graphics.draw(enemy.image,enemy.x,enemy.y,angle,1,1,25,25)
 
 end
 
